d634ed1ad6060db1146099e8a87d7d3c9c4d7a70
Jessica Moraes Ferreira <jessica.moraes@venturus.org.br>
2023-02-06 16:29:41 -0300

    NestJS

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'postgres',
+  autoLoadEntities: true,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
